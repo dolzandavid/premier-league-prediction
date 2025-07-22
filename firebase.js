2a55722beac1b17a3d312171be807cec047ded83
dolzandavid <david.dolzan@gmail.com>
2025-07-22 12:37:51 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
-  getFirestore, doc, setDoc, getDoc, collection, getDocs, updateDoc
+  getFirestore, doc, setDoc, getDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -15,73 +15,12 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-async function saveData(collectionName, docId, data) {
-  await setDoc(doc(db, collectionName, docId), data);
+export async function savePredictions(gw, player, preds) {
+  await setDoc(doc(db, "predictions", `gw_${gw}_${player}`), { predictions: preds });
 }
 
-async function updateData(collectionName, docId, data) {
-  await updateDoc(doc(db, collectionName, docId), data);
-}
-
-async function getData(collectionName, docId) {
-  const ref = doc(db, collectionName, docId);
+export async function getPredictions(gw, player) {
+  const ref = doc(db, "predictions", `gw_${gw}_${player}`);
   const snap = await getDoc(ref);
-  return snap.exists() ? snap.data() : null;
-}
-
-async function getAllDocs(collectionName) {
-  const snap = await getDocs(collection(db, collectionName));
-  return snap.docs.map(doc => ({ id: doc.id, data: doc.data() }));
-}
-
-window.firebaseHelpers = {
-  saveData,
-  getData,
-  getAllDocs,
-  updateData
-};
-
-window.getFixtures = async function(gw) {
-  const data = await firebaseHelpers.getData("fixtures", `gw_${gw}`);
-  return data ? data.fixtures : [];
-}
-
-window.saveFixtures = async function(gw, fixtures) {
-  await firebaseHelpers.saveData("fixtures", `gw_${gw}`, { fixtures });
-}
-
-window.getPredictions = async function(gw, player) {
-  const data = await firebaseHelpers.getData("predictions", `gw_${gw}_${player}`);
-  return data ? data.predictions : [];
-}
-
-window.savePredictions = async function(gw, player, preds) {
-  await firebaseHelpers.saveData("predictions", `gw_${gw}_${player}`, { predictions: preds });
-}
-
-window.getResults = async function(gw) {
-  const data = await firebaseHelpers.getData("results", `gw_${gw}`);
-  return data ? data.results : [];
-}
-
-window.saveResults = async function(gw, results) {
-  await firebaseHelpers.saveData("results", `gw_${gw}`, { results });
-}
-
-window.getDeadline = async function(gw) {
-  const data = await firebaseHelpers.getData("deadlines", `gw_${gw}`);
-  return data ? data.deadline : "";
-}
-
-window.saveDeadline = async function(gw, dateStr) {
-  await firebaseHelpers.saveData("deadlines", `gw_${gw}`, { deadline: dateStr });
-}
-
-window.getPassword = async function(player) {
-  const data = await firebaseHelpers.getData("passwords", player);
-  return data ? data.password : null;
-}
-
-window.savePassword = async function(player, pw) {
-  await firebaseHelpers.saveData("passwords", player, { password: pw });
+  return snap.exists() ? snap.data().predictions : [];
 }
