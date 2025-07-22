73c21cc9272b6980a32fa3bcfb0b58e42f9c61f4
dolzandavid <david.dolzan@gmail.com>
2025-07-22 12:22:01 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA2WG0LUwLCDqmylgezCZ0aH5Q8vTaXTKU",
   authDomain: "pl-predictor-13a6d.firebaseapp.com",
   projectId: "pl-predictor-13a6d",
-  storageBucket: "pl-predictor-13a6d.firebasestorage.app",
+  storageBucket: "pl-predictor-13a6d.appspot.com",
   messagingSenderId: "286908896536",
   appId: "1:286908896536:web:dabd76b4faaa72a98cfa6d"
 };
@@ -34,7 +34,6 @@ async function getAllDocs(collectionName) {
   return snap.docs.map(doc => ({ id: doc.id, data: doc.data() }));
 }
 
-// Define globally accessible functions
 window.firebaseHelpers = {
   saveData,
   getData,
@@ -45,44 +44,44 @@ window.firebaseHelpers = {
 window.getFixtures = async function(gw) {
   const data = await firebaseHelpers.getData("fixtures", `gw_${gw}`);
   return data ? data.fixtures : [];
-};
+}
 
 window.saveFixtures = async function(gw, fixtures) {
   await firebaseHelpers.saveData("fixtures", `gw_${gw}`, { fixtures });
-};
+}
 
 window.getPredictions = async function(gw, player) {
   const data = await firebaseHelpers.getData("predictions", `gw_${gw}_${player}`);
   return data ? data.predictions : [];
-};
+}
 
 window.savePredictions = async function(gw, player, preds) {
   await firebaseHelpers.saveData("predictions", `gw_${gw}_${player}`, { predictions: preds });
-};
+}
 
 window.getResults = async function(gw) {
   const data = await firebaseHelpers.getData("results", `gw_${gw}`);
   return data ? data.results : [];
-};
+}
 
 window.saveResults = async function(gw, results) {
   await firebaseHelpers.saveData("results", `gw_${gw}`, { results });
-};
+}
 
 window.getDeadline = async function(gw) {
   const data = await firebaseHelpers.getData("deadlines", `gw_${gw}`);
   return data ? data.deadline : "";
-};
+}
 
 window.saveDeadline = async function(gw, dateStr) {
   await firebaseHelpers.saveData("deadlines", `gw_${gw}`, { deadline: dateStr });
-};
+}
 
 window.getPassword = async function(player) {
   const data = await firebaseHelpers.getData("passwords", player);
   return data ? data.password : null;
-};
+}
 
 window.savePassword = async function(player, pw) {
   await firebaseHelpers.saveData("passwords", player, { password: pw });
-};
+}
